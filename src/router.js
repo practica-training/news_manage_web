c06a8a00a3c9a6cf34ca95a64db92e35483b28a1
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2019-11-25 20:56:41 +0800

    增加进行评价的组件的路由(测试用)
    增加进行评价的组件的路由(测试用)

--- src/router.js
+++ src/router.js
@@ -60,6 +60,14 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/TeacherPlus.vue')
     },
+    {
+      path: '/comment',
+      name: '进行评价的组件',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './components/NewsComment.vue')
+    },
     {//上面的都不匹配，则跳转到404
       path: '*',
       name: 'notFound',
