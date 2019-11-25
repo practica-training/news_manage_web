f3d733af97890e79c5b7e50056e825ef06d81715
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2019-11-25 20:56:20 +0800

    新增评论组件和评论展示组件

---
+++ src/components/NewsComment.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <el-button @click="drawer = true" type="default" style="margin-left: 16px;">
+            评论
+        </el-button>
+        <el-drawer modal size="60%"
+                title="评论"
+                :visible.sync="drawer"
+                :direction="direction"
+                :before-close="handleClose">
+            <span>来评论吧!</span>
+        </el-drawer>
+    </div>
+</template>
+
+<script>
+    import {Button, Drawer} from "element-ui";
+
+    export default {
+        name: "NewsComment",
+        components: {
+            [Button.name]: Button,
+            [Drawer.name]: Drawer
+        },
+        data() {
+            return {
+                drawer: false,
+                direction: 'btt',
+            }
+        },
+        methods: {
+            handleClose(done) {
+                this.$confirm('是否要取消评论？')
+                    .then(_ => {
+                        done();
+                    })
+                    .catch(_ => {});
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/NewsShowComment.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>展示评论的组件</div>
+</template>
+
+<script>
+    export default {
+        name: "NewsShowComment"
+    }
+</script>
+
+<style scoped>
+
+</style>
